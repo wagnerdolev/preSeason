302c69db7a1749e7a6f0c9f9d44045a392255d1c
DolevWagner <wagnerdolev@gmail.com>
2023-10-17 20:18:23 +0300

    add Changes

--- preSeason/src/main/java/frc/robot/SubSystems/Chassis.java
+++ preSeason/src/main/java/frc/robot/SubSystems/Chassis.java
@@ -11,23 +11,23 @@ import com.kauailabs.navx.frc.AHRS;
 
 public class Chassis extends SubsystemBase{
 
-    private static CANSparkMax m_rightForward, m_rightMid, m_rightBack, m_leftForward, m_leftMid, m_leftBack;
-    private static MotorControllerGroup m_rightMotorGroup, m_leftMotorGroup;
+    private static CANSparkMax m_rightForwardChassisMotor, m_rightMidChassisMotor, m_rightBackChassisMotor, m_leftForwardChassisMotor, m_leftMidChassisMotor, m_leftBackChassisMotor;
+    private static MotorControllerGroup m_rightChassisMotorGroup, m_leftChassisMotorGroup;
     private static Chassis m_chassis;
-    public static AHRS m_navX;
+    public static AHRS m_ChassisNavXGyro;
     
     public Chassis() {
-        m_navX = new AHRS();
+        m_ChassisNavXGyro = new AHRS();
 
-        m_rightForward = new CANSparkMax(Consts.ChassisConsts.RIGHT_FORWARD_CHASSIS_MOTOR_ID, MotorType.kBrushless);
-        m_rightMid = new CANSparkMax(Consts.ChassisConsts.RIGHT_MID_CHASSIS_MOTOR_ID, MotorType.kBrushless);
-        m_rightBack = new CANSparkMax(Consts.ChassisConsts.RIGHT_BACK_CHASSIS_MOTOR_ID, MotorType.kBrushless);
-        m_leftForward = new CANSparkMax(Consts.ChassisConsts.LEFT_FORWARD_CHASSIS_MOTOR_ID, MotorType.kBrushless);
-        m_leftForward = new CANSparkMax(Consts.ChassisConsts.LEFT_MID_CHASSIS_MOTOR_ID, MotorType.kBrushless);
-        m_leftBack = new CANSparkMax(Consts.ChassisConsts.LEFT_BACK_CHASSIS_MOTOR_ID, MotorType.kBrushless);
+        m_rightForwardChassisMotor = new CANSparkMax(Consts.ChassisConsts.RIGHT_FORWARD_CHASSIS_MOTOR_ID, MotorType.kBrushless);
+        m_rightMidChassisMotor = new CANSparkMax(Consts.ChassisConsts.RIGHT_MID_CHASSIS_MOTOR_ID, MotorType.kBrushless);
+        m_rightBackChassisMotor = new CANSparkMax(Consts.ChassisConsts.RIGHT_BACK_CHASSIS_MOTOR_ID, MotorType.kBrushless);
+        m_leftForwardChassisMotor = new CANSparkMax(Consts.ChassisConsts.LEFT_FORWARD_CHASSIS_MOTOR_ID, MotorType.kBrushless);
+        m_leftForwardChassisMotor = new CANSparkMax(Consts.ChassisConsts.LEFT_MID_CHASSIS_MOTOR_ID, MotorType.kBrushless);
+        m_leftBackChassisMotor = new CANSparkMax(Consts.ChassisConsts.LEFT_BACK_CHASSIS_MOTOR_ID, MotorType.kBrushless);
 
-        m_rightMotorGroup = new MotorControllerGroup(m_leftBack, m_leftMid, m_leftForward);
-        m_leftMotorGroup = new MotorControllerGroup(m_rightBack, m_rightMid, m_rightForward);
+        m_rightChassisMotorGroup = new MotorControllerGroup(m_leftBackChassisMotor, m_leftMidChassisMotor, m_leftForwardChassisMotor);
+        m_leftChassisMotorGroup = new MotorControllerGroup(m_rightBackChassisMotor, m_rightMidChassisMotor, m_rightForwardChassisMotor);
     }
 
     public static void getChassis(){
@@ -36,34 +36,34 @@ public class Chassis extends SubsystemBase{
     }
 
     public static void drive(double rSpeed, double lSpeed){
-        m_rightMotorGroup.set(rSpeed);
-        m_leftMotorGroup.set(lSpeed);
+        m_rightChassisMotorGroup.set(rSpeed);
+        m_leftChassisMotorGroup.set(lSpeed);
     }
 
     public static void setMode(IdleMode idleMode){
-        m_rightForward.setIdleMode(idleMode);
-        m_rightMid.setIdleMode(idleMode);
-        m_rightBack.setIdleMode(idleMode);
-        m_leftForward.setIdleMode(idleMode);
-        m_leftMid.setIdleMode(idleMode);
-        m_leftBack.setIdleMode(idleMode);
+        m_rightForwardChassisMotor.setIdleMode(idleMode);
+        m_rightMidChassisMotor.setIdleMode(idleMode);
+        m_rightBackChassisMotor.setIdleMode(idleMode);
+        m_leftForwardChassisMotor.setIdleMode(idleMode);
+        m_leftMidChassisMotor.setIdleMode(idleMode);
+        m_leftBackChassisMotor.setIdleMode(idleMode);
 
     }
     public static double getRightDistance(){
-        return (m_rightForward.getEncoder().getPosition() * Consts.ChassisConsts.POSITION_TO_DISTANCE);
+        return (m_rightForwardChassisMotor.getEncoder().getPosition() * Consts.ChassisConsts.POSITION_TO_DISTANCE);
     }
 
     public static double getLeftDistance(){
-        return (m_leftForward.getEncoder().getPosition() * Consts.ChassisConsts.POSITION_TO_DISTANCE);
+        return (m_leftForwardChassisMotor.getEncoder().getPosition() * Consts.ChassisConsts.POSITION_TO_DISTANCE);
     }
     
     public static AHRS getGyro(){
-        return m_navX;
+        return m_ChassisNavXGyro;
     }
 
     public static void stopChassis(){
-        m_rightMotorGroup.set(0);
-        m_leftMotorGroup.set(0);
+        m_rightChassisMotorGroup.set(0);
+        m_leftChassisMotorGroup.set(0);
     }
     
 }
